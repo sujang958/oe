4cd8461ba22d22e5722ddf12f2b5d8b7db829919
sujang958 <sujangdev@gmail.com>
2024-06-20 22:23:27 +0900

    feat: add a hero text and some options

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,5 @@
+<button
+  type="button"
+  class="rounded-lg px-2.5 py-0.5 text-sm bg-neutral-200 text-neutral-800 font-medium flex flex-row items-center gap-x-1.5 {$$props.class}"
+  ><slot />
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,27 @@
-sup bitch
+<script>
+  import Button from "$lib/components/button.svelte"
+</script>
+
+<div class="flex flex-col p-16">
+  <p class="text-3xl font-bold">My wordlists</p>
+  <div class="py-1"></div>
+  <div class="py-6 flex flex-row items-center gap-x-2">
+    <Button
+      ><svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="w-4 h-4 stroke-neutral-800"
+        ><path d="m21 16-4 4-4-4" /><path d="M17 20V4" /><path d="m3 8 4-4 4 4" /><path
+          d="M7 4v16"
+        /></svg
+      >Last praticed
+    </Button>
+  </div>
+</div>
