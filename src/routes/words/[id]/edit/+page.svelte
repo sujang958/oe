f4d9c1c75fd5903399eeebc1e5811fd6f46ba0e6
sujang958 <enter7377@naver.com>
2024-06-24 14:44:09 +0900

    feat: add 90% of the edit page

--- src/routes/words/[id]/edit/+page.svelte
+++ src/routes/words/[id]/edit/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import Button from "$lib/components/Button.svelte"
   import Input from "$lib/components/Input.svelte"
-  import { ChevronLeft, MoveLeft } from "lucide-svelte"
+  import { ChevronLeft, MoveLeft, PlusIcon } from "lucide-svelte"
 
   // TODO: add "press / to focus"
 </script>
@@ -9,20 +9,35 @@
 <div class="flex flex-col px-16 pt-12 pb-16 max-w-2xl w-full self-center min-h-screen">
   <header class="flex flex-row items-center justify-between">
     <Button class="bg-transparent  -ml-2"><ChevronLeft class="h-5 w-5" /> Go Back</Button>
-    <p class="text-base font-meidum">Japanese Restaurants</p>
   </header>
 
   <div class="py-5"></div>
 
-  <div class="flex-1 flex flex-col items-center justify-center">
-    <p class="text-base font-medium text-neutral-500 -mt-8">1 / 21</p>
-    <div class="py-4"></div>
-    <p class="text-5xl font-bold"><ruby>下</ruby>さい</p>
-    <div class="py-5"></div>
-    <Input class="w-56" placeholder="Type your answer" />
+  <div class="flex flex-col py-6 gap-y-4">
+    <!-- svelte-ignore a11y-label-has-associated-control -->
+    <label class="flex flex-col gap-y-1.5">
+      <p class="text-sm">Title</p>
+      <Input value="Japanese Restaurants" />
+    </label>
+
+    <div class="py-4"><hr /></div>
+
+    <div class="flex flex-row items-center gap-x-6 justify-between">
+      <p class="text-xl text-balance">すみません</p>
+      <p class="text-lg text-balance text-right max-w-96">
+        excuse me; pardon me; I'm sorry; I beg your pardon
+      </p>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 justify-between">
+      <p class="text-xl text-balance" contenteditable>
+        <ruby>下<rt>くだ</rt></ruby>さい
+      </p>
+      <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+    </div>
+    <Button class="justify-center text-base mt-6 bg-neutral-100">+</Button>
   </div>
 
-  <div class="flex flex-row items-center gap-x-3 -mb-4">
+  <div class="mt-auto flex flex-row items-center gap-x-3 -mb-4">
     <Button class="flex-1 justify-center py-1.5 text-base">Cancel</Button>
     <Button class="flex-1 justify-center py-1.5 text-base bg-green-300 text-green-950">Save</Button>
   </div>
