1f1ca032e26efc651bb8af1683c8ef6e90c6305c
sujang958 <enter7377@naver.com>
2024-07-08 08:23:04 +0900

    feat: add the edit page

--- src/routes/words/[id]/edit/+page.svelte
+++ src/routes/words/[id]/edit/+page.svelte
@@ -24,15 +24,15 @@
   <div class="flex flex-col py-6 gap-y-4">
     <!-- svelte-ignore a11y-label-has-associated-control -->
     <label class="flex flex-col gap-y-1.5">
-      <p class="text-sm">Title</p>
+      <p class="text-sm font-medium">Title</p>
       <Input value="Japanese Restaurants" />
     </label>
 
     <div class="mt-4">
-      <p class="text-sm">Words</p>
+      <p class="text-sm font-medium">Words</p>
       <div class="py-3 flex flex-row items-center gap-x-3">
-        <!-- <input type="checkbox" class="rounded" id="" /> -->
-        <Button class="bg-neutral-100">Select All</Button>
+        <input type="checkbox" class="rounded w-3.5 h-3.5" id="" />
+        <!-- <Button class="bg-neutral-100">Select All</Button> -->
         <Button class="bg-neutral-100">Deselect All</Button>
         <Button class="bg-neutral-100">Undo</Button>
         <Button class="bg-red-200 text-red-900">Delete</Button>
@@ -66,20 +66,60 @@
       </Swipe>
     </div> -->
 
-    <div class="flex flex-row items-center gap-x-6">
-      <input type="checkbox" class="rounded" name="" id="" />
-      <div class="flex flex-row items-center justify-between gap-x-4 w-full">
-        <p class="text-xl text-balance" contenteditable>
-          <ruby>下<rt>くだ</rt></ruby>さい
-        </p>
-        <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+    <section class="flex flex-col gap-y-4">
+      <div class="flex flex-row items-center gap-x-6">
+        <input type="checkbox" class="rounded" name="" id="" />
+        <div class="flex flex-row items-center justify-between gap-x-4 w-full">
+          <p class="text-xl text-balance" contenteditable>
+            <ruby>下<rt>くだ</rt></ruby>さい
+          </p>
+          <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+        </div>
       </div>
-    </div>
+      <div class="flex flex-row items-center gap-x-6">
+        <input type="checkbox" class="rounded" name="" id="" />
+        <div class="flex flex-row items-center justify-between gap-x-4 w-full">
+          <p class="text-xl text-balance" contenteditable>
+            <ruby>下<rt>くだ</rt></ruby>さい
+          </p>
+          <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center gap-x-6">
+        <input type="checkbox" class="rounded" name="" id="" />
+        <div class="flex flex-row items-center justify-between gap-x-4 w-full">
+          <p class="text-xl text-balance" contenteditable>
+            <ruby>下<rt>くだ</rt></ruby>さい
+          </p>
+          <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center gap-x-6">
+        <input type="checkbox" class="rounded" name="" id="" />
+        <div class="flex flex-row items-center justify-between gap-x-4 w-full">
+          <p class="text-xl text-balance" contenteditable>
+            <ruby>下<rt>くだ</rt></ruby>さい
+          </p>
+          <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+        </div>
+      </div>
+      <div class="flex flex-row items-center gap-x-6">
+        <input type="checkbox" class="rounded" name="" id="" />
+        <div class="flex flex-row items-center justify-between gap-x-4 w-full">
+          <p class="text-xl text-balance" contenteditable>
+            <ruby>下<rt>くだ</rt></ruby>さい
+          </p>
+          <p class="text-lg text-balance text-right max-w-96" contenteditable>please (give me)</p>
+        </div>
+      </div>
+    </section>
 
-    <Button class="justify-center text-base mt-6 bg-neutral-100">+</Button>
+    <Button class="justify-center text-base mt-6 bg-neutral-100 py-0.5">+</Button>
   </div>
+</div>
 
-  <div class="mt-auto flex flex-row items-center gap-x-3 -mb-4">
+<div class="fixed left-0 bottom-4 w-full flex flex-col items-center">
+  <div class="max-w-xl px-4 w-full flex flex-row items-center gap-x-3">
     <Button class="flex-1 justify-center py-1.5 text-base">Cancel</Button>
     <Button class="flex-1 justify-center py-1.5 text-base bg-green-300 text-green-950">Save</Button>
   </div>
