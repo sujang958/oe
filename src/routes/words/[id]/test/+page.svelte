3fbf6c86fb214f0aca4e106f9f12499971f5ed0c
sujang958 <enter7377@naver.com>
2024-07-10 12:54:16 +0900

    feat: componentrize

--- src/routes/words/[id]/test/+page.svelte
+++ src/routes/words/[id]/test/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+  import BottomArea from "$lib/components/BottomArea.svelte"
   import Button from "$lib/components/Button.svelte"
-    import Input from "$lib/components/Input.svelte"
+  import Input from "$lib/components/Input.svelte"
   import { ChevronLeft, MoveLeft } from "lucide-svelte"
 
   // TODO: add "press / to focus"
@@ -21,9 +22,9 @@
     <div class="py-5"></div>
     <Input class="w-56" placeholder="Type your answer" />
   </div>
-
-  <div class="flex flex-row items-center gap-x-3 -mb-4">
-    <Button class="flex-1 justify-center py-1.5 text-base">Skip</Button>
-    <Button class="flex-1 justify-center py-1.5 text-base bg-blue-400 text-blue-900">Check</Button>
-  </div>
 </div>
+
+<BottomArea>
+  <Button class="flex-1 justify-center py-1.5 text-base">Skip</Button>
+  <Button class="flex-1 justify-center py-1.5 text-base bg-blue-400 text-blue-900">Check</Button>
+</BottomArea>
