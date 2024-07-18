35e7572bfe63c9eca01ab50800b85b8ed7545850
sujang958 <sujangdev@gmail.com>
2024-07-18 22:24:27 +0900

    ref: add a missing component

---
+++ src/lib/components/TopArea.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { twMerge } from "tailwind-merge"
+</script>
+
+<header class="fixed left-0 top-6 w-full flex flex-col items-center">
+  <div class={twMerge("max-w-xl px-4 w-full flex flex-row items-center gap-x-3", $$props.class)}>
+    <slot />
+  </div>
+</header>
