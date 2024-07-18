6c5fce2b8f0b3cfe70f5236a79f78a79786729a5
sujang958 <sujangdev@gmail.com>
2024-07-18 21:47:49 +0900

    feat: apply basic structures

--- src/lib/components/GoBackButton.svelte
+++ src/lib/components/GoBackButton.svelte
@@ -1,12 +1,14 @@
 <script>
   import { ChevronLeft } from "lucide-svelte"
-
   import Button from "./Button.svelte"
-    import { twMerge } from "tailwind-merge"
+  import { twMerge } from "tailwind-merge"
+  import { goto } from "$app/navigation"
+
+  export let where = ""
 </script>
 
 <Button
-  on:click={() => history.back()}
-  class={twMerge("bg-transparent hover:bg-neutral-100 w-28 justify-center py-1 -ml-2 -mt-3", $$props.class)}
-  ><ChevronLeft class="h-5 w-5" /> Go Back</Button
+  on:click={() => (where ? goto(where) : history.back())}
+  class={twMerge("bg-transparent hover:bg-neutral-100 py-1", $$props.class)}
+  ><ChevronLeft class="h-4 w-4" /> Go Back</Button
 >
--- src/routes/words/[id]/test/+page.svelte
+++ src/routes/words/[id]/test/+page.svelte
@@ -1,24 +1,34 @@
 <script lang="ts">
+  import { page } from "$app/stores"
   import BottomArea from "$lib/components/BottomArea.svelte"
   import Button from "$lib/components/Button.svelte"
+  import GoBackButton from "$lib/components/GoBackButton.svelte"
   import Input from "$lib/components/Input.svelte"
-  import { ChevronLeft, MoveLeft } from "lucide-svelte"
+  import TopArea from "$lib/components/TopArea.svelte"
+  import { WordLists } from "$lib/stores/WordList"
+
+  const id = $page.params.id
+  const wordlist = $WordLists[id] ?? { id: -1, title: "", words: [] }
+
+  let currentWordIndex = 0
 
   // TODO: add "press / to focus"
 </script>
 
 <div class="flex flex-col px-16 pt-12 pb-16 max-w-2xl w-full self-center min-h-screen">
-  <header class="flex flex-row items-center justify-between">
-    <Button on:click={() => history.back()} class="bg-transparent hover:underline"><ChevronLeft class="h-5 w-5" /> Go Back</Button>
-    <p class="text-base font-meidum">Japanese Restaurants</p>
-  </header>
+  <TopArea class="justify-between">
+    <GoBackButton where="/wordlist/${id}" />
+    <p class="text-base font-meidum">{wordlist.title}</p>
+  </TopArea>
 
   <div class="py-5"></div>
 
   <div class="flex-1 flex flex-col items-center justify-center">
-    <p class="text-base font-medium text-neutral-500 -mt-8">1 / 21</p>
+    <p class="text-base font-medium text-neutral-500 -mt-8">
+      {currentWordIndex + 1} / {wordlist.words.length}
+    </p>
     <div class="py-4"></div>
-    <p class="text-5xl font-bold"><ruby>下</ruby>さい</p>
+    <p class="text-5xl font-bold">{wordlist.words[0].word}</p>
     <div class="py-5"></div>
     <Input class="w-56" placeholder="Type your answer" />
   </div>
